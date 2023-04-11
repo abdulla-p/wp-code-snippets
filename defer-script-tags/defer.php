db532f005aa089989e91d16d3b1fd5f65d4add6b
Abdulla Patva <100014266+abdulla-p@users.noreply.github.com>
2023-04-11 12:30:38 +0530

    Update defer.php

--- defer-script-tags/defer.php
+++ defer-script-tags/defer.php
@@ -1,9 +1,13 @@
 <?php
+/** 
+ * Code Snippet: Add defer or async attributes to js script tags
+ */
 function add_defer_attribute( $tag, $handle ) {
 	// Add script handles to the array below.
-	// Similarly you can add async attribute as well. Or any other desired attribute of yours.
 	$scripts_to_defer = array( 'script-handle-1', 'script-handle-2', 'script-handle-3' );
+	
 	if ( in_array( $scripts_to_defer, $handle, true ) ) {
+		// Similarly you can add async attribute as well. Or any other desired attribute of yours.
 		return str_replace( ' src', ' defer="defer" src', $tag );
 	}
 	return $tag;
